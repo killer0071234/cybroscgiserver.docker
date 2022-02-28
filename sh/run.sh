0dbaeeb867a6a215f6f83157a959f5eb599896b4
Daniel Gangl <killer007@gmx.at>
2022-02-28 19:00:42 +0100

    Add nginx with content xml header at port 80

--- sh/run.sh
+++ sh/run.sh
@@ -4,6 +4,8 @@
 # @Last Modified by:   Daniel Gangl
 # @Last Modified time: 2021-07-11 23:01:01
 set -e
+echo "Starting nginx server..."
+nginx
 cd /usr/local/bin/scgi_server/src
 echo "Starting cybroscgiserver..."
 python app.py
